d27b9fe8e6ce4115c300101204d80e6f96bfb05f
saad-Alqattan <eng.elect.saadamer@gmail.com>
2023-12-04 20:34:30 +0300

    05-setting-timers-managing-state

--- src/components/Player.jsx
+++ src/components/Player.jsx
@@ -4,7 +4,7 @@ export default function Player() {
   const playerName = useRef();
 
   const [enteredPlayerName, setEnteredPlayerName] = useState(null);
-  
+
   function handleClick() {
     setEnteredPlayerName(playerName.current.value);
     playerName.current.value = '';
@@ -14,10 +14,7 @@ export default function Player() {
     <section id="player">
       <h2>Welcome {enteredPlayerName ?? 'unknown entity'}</h2>
       <p>
-        <input
-          ref={playerName}
-          type="text"
-        />
+        <input ref={playerName} type="text" />
         <button onClick={handleClick}>Set Name</button>
       </p>
     </section>
