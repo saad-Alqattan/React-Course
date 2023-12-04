dfe7a6ab83072780f5b01be751bf92bce57edd65
saad-Alqattan <eng.elect.saadamer@gmail.com>
2023-12-04 17:57:37 +0300

    03-finished

--- src/components/Results.jsx
+++ src/components/Results.jsx
@@ -1,18 +1,11 @@
 import { calculateInvestmentResults, formatter } from '../util/investment.js';
 
-
 export default function Results({ input }) {
-  const results = [];
-  calculateInvestmentResults(input, results);
-
-  if (results.length === 0) {
-    return <p className="center">Invalid input data provided.</p>
-  }
-
+  const resultsData = calculateInvestmentResults(input);
   const initialInvestment =
-    results[0].valueEndOfYear -
-    results[0].interest -
-    results[0].annualInvestment;
+    resultsData[0].valueEndOfYear -
+    resultsData[0].interest -
+    resultsData[0].annualInvestment;
 
   return (
     <table id="result">
@@ -26,7 +19,7 @@ export default function Results({ input }) {
         </tr>
       </thead>
       <tbody>
-        {results.map((yearData) => {
+        {resultsData.map((yearData) => {
           const totalInterest =
             yearData.valueEndOfYear -
             yearData.annualInvestment * yearData.year -
--- src/util/investment.js
+++ src/util/investment.js
@@ -9,19 +9,22 @@ export function calculateInvestmentResults({
   annualInvestment,
   expectedReturn,
   duration,
-}, results) {
+}) {
+  const annualData = [];
   let investmentValue = initialInvestment;
 
   for (let i = 0; i < duration; i++) {
     const interestEarnedInYear = investmentValue * (expectedReturn / 100);
     investmentValue += interestEarnedInYear + annualInvestment;
-    results.push({
+    annualData.push({
       year: i + 1, // year identifier
       interest: interestEarnedInYear, // the amount of interest earned in this year
       valueEndOfYear: investmentValue, // investment value at end of year
       annualInvestment: annualInvestment, // investment added in this year
     });
   }
+
+  return annualData;
 }
 
 // The browser-provided Intl API is used to prepare a formatter object
