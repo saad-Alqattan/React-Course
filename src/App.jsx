017fdf26b6cc1dcf84cf0104513fb2af25adc8f5
saad-Alqattan <eng.elect.saadamer@gmail.com>
2023-12-06 18:21:57 +0300

    01-starting-project-Contex

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,77 @@
 import { useState } from 'react';
 
 import Header from './components/Header.jsx';
-import UserInput from './components/UserInput.jsx';
-import Results from './components/Results.jsx';
+import Shop from './components/Shop.jsx';
+import { DUMMY_PRODUCTS } from './dummy-products.js';
 
 function App() {
-  const [userInput, setUserInput] = useState({
-    initialInvestment: 10000,
-    annualInvestment: 1200,
-    expectedReturn: 6,
-    duration: 10,
+  const [shoppingCart, setShoppingCart] = useState({
+    items: [],
   });
 
-  function handleChange(inputIdentifier, newValue) {
-    setUserInput((prevUserInput) => {
+  function handleAddItemToCart(id) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+
+      const existingCartItemIndex = updatedItems.findIndex(
+        (cartItem) => cartItem.id === id
+      );
+      const existingCartItem = updatedItems[existingCartItemIndex];
+
+      if (existingCartItem) {
+        const updatedItem = {
+          ...existingCartItem,
+          quantity: existingCartItem.quantity + 1,
+        };
+        updatedItems[existingCartItemIndex] = updatedItem;
+      } else {
+        const product = DUMMY_PRODUCTS.find((product) => product.id === id);
+        updatedItems.push({
+          id: id,
+          name: product.title,
+          price: product.price,
+          quantity: 1,
+        });
+      }
+
       return {
-        ...prevUserInput,
-        [inputIdentifier]: newValue,
+        items: updatedItems,
+      };
+    });
+  }
+
+  function handleUpdateCartItemQuantity(productId, amount) {
+    setShoppingCart((prevShoppingCart) => {
+      const updatedItems = [...prevShoppingCart.items];
+      const updatedItemIndex = updatedItems.findIndex(
+        (item) => item.id === productId
+      );
+
+      const updatedItem = {
+        ...updatedItems[updatedItemIndex],
+      };
+
+      updatedItem.quantity += amount;
+
+      if (updatedItem.quantity <= 0) {
+        updatedItems.splice(updatedItemIndex, 1);
+      } else {
+        updatedItems[updatedItemIndex] = updatedItem;
+      }
+
+      return {
+        items: updatedItems,
       };
     });
   }
 
   return (
     <>
-      <Header />
-      <UserInput userInput={userInput} onChange={handleChange} />
-      <Results input={userInput} />
+      <Header
+        cart={shoppingCart}
+        onUpdateCartItemQuantity={handleUpdateCartItemQuantity}
+      />
+      <Shop onAddItemToCart={handleAddItemToCart} />
     </>
   );
 }
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,45 @@
-import logo from '../assets/investment-calculator-logo.png';
+import { useRef } from 'react';
 
-export default function Header() {
+import CartModal from './CartModal.jsx';
+
+export default function Header({ cart, onUpdateCartItemQuantity }) {
+  const modal = useRef();
+
+  const cartQuantity = cart.items.length;
+
+  function handleOpenCartClick() {
+    modal.current.open();
+  }
+
+  let modalActions = <button>Close</button>;
+
+  if (cartQuantity > 0) {
+    modalActions = (
+      <>
+        <button>Close</button>
+        <button>Checkout</button>
+      </>
+    );
+  }
+
   return (
-    <header id="header">
-      <img src={logo} alt="Logo showing a money bag" />
-      <h1>Investment Calculator</h1>
-    </header>
+    <>
+      <CartModal
+        ref={modal}
+        cartItems={cart.items}
+        onUpdateCartItemQuantity={onUpdateCartItemQuantity}
+        title="Your Cart"
+        actions={modalActions}
+      />
+      <header id="main-header">
+        <div id="main-title">
+          <img src="logo.png" alt="Elegant model" />
+          <h1>Elegant Context</h1>
+        </div>
+        <p>
+          <button onClick={handleOpenCartClick}>Cart ({cartQuantity})</button>
+        </p>
+      </header>
+    </>
   );
 }
