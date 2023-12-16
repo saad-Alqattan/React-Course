814662b38bd1879c6c5de3496972b9beb5b5f1aa
saad-Alqattan <eng.elect.saadamer@gmail.com>
2023-12-16 19:49:58 +0300

    start-project-input

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,14 @@
+import Header from './components/Header.jsx';
+import Login from './components/Login.jsx';
+
 function App() {
   return (
-    <div>
-      <header>
-        <img src="src/assets/react-core-concepts.png" alt="Stylized atom" />
-        <h1>React Essentials</h1>
-        <p>
-          Fundamental React concepts you will need for almost any app you are
-          going to build!
-        </p>
-      </header>
+    <>
+      <Header />
       <main>
-        <h2>Time to get started!</h2>
+        <Login />
       </main>
-    </div>
+    </>
   );
 }
 
