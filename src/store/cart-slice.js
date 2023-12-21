f7823fd4136a33d4fdd7808ed37c9c31e672a45b
saad-Alqattan <eng.elect.saadamer@gmail.com>
2023-12-21 20:18:24 +0300

    zz-suboptimal-example-code

--- src/store/cart-slice.js
+++ src/store/cart-slice.js
@@ -5,7 +5,6 @@ const cartSlice = createSlice({
   initialState: {
     items: [],
     totalQuantity: 0,
-    changed: false,
   },
   reducers: {
     replaceCart(state, action) {
@@ -16,7 +15,6 @@ const cartSlice = createSlice({
       const newItem = action.payload;
       const existingItem = state.items.find((item) => item.id === newItem.id);
       state.totalQuantity++;
-      state.changed = true;
       if (!existingItem) {
         state.items.push({
           id: newItem.id,
@@ -34,12 +32,10 @@ const cartSlice = createSlice({
       const id = action.payload;
       const existingItem = state.items.find((item) => item.id === id);
       state.totalQuantity--;
-      state.changed = true;
       if (existingItem.quantity === 1) {
         state.items = state.items.filter((item) => item.id !== id);
       } else {
         existingItem.quantity--;
-        existingItem.totalPrice = existingItem.totalPrice - existingItem.price;
       }
     },
   },
