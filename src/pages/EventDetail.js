d529d9043f562133764ea35db1daad0cc0d2a70f
saad-Alqattan <eng.elect.saadamer@gmail.com>
2023-12-23 16:42:31 +0300

    23-userouteloaderdata

--- src/pages/EventDetail.js
+++ src/pages/EventDetail.js
@@ -1,26 +1,27 @@
-import { useLoaderData, json } from 'react-router-dom';
+import { useRouteLoaderData, json } from 'react-router-dom';
 
 import EventItem from '../components/EventItem';
 
 function EventDetailPage() {
-  const data = useLoaderData();
+  const data = useRouteLoaderData('event-detail');
 
-  return (
-    <EventItem event={data.event} />
-  );
+  return <EventItem event={data.event} />;
 }
 
 export default EventDetailPage;
 
-export async function loader({request, params}) {
+export async function loader({ request, params }) {
   const id = params.eventId;
 
   const response = await fetch('http://localhost:8080/events/' + id);
 
   if (!response.ok) {
-    throw json({message: 'Could not fetch details for selected event.'}, {
-      status: 500
-    })
+    throw json(
+      { message: 'Could not fetch details for selected event.' },
+      {
+        status: 500,
+      }
+    );
   } else {
     return response;
   }
